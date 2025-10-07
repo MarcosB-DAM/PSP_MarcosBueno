2c580c0af3d112c841efeb0f3744cd784e3bf3bf
MarcosB-DAM <mbuenobuj@gmail.com>
2025-10-07 10:33:52 +0200

    Ejercicios Fifo1

---
+++ EjsFifo1/fifo22.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+
+int main(){
+
+	return 0;
+}
